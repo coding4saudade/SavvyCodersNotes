66f82cd39807980a71eb24724e9f9fa425f355f7
coding4saudade <61851617+coding4saudade@users.noreply.github.com>
2022-10-21 15:45:50 -0500

    "updating notes"

--- Class_10_20_notes.py
+++ Class_10_20_notes.py
@@ -1,3 +1,12 @@
 #Notes for Savvy Coders Class 10/20
 
-#
+#Practice Git - so I can upload notes for each class
+#Having issues creating a repo
+#Watch this video:
+# https://www.youtube.com/watch?v=8Dd7KRpKeaE&ab_channel=CoderCoder
+
+""""Testing out test blocks
+Angela is really good at coding. Glad she is helping me
+
+W00t
+"""
